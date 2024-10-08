73aba244e418de53e26cc260e315d6b5bf05b917
Ildar Khalikov <rajistangosh@gmail.com>
2024-10-08 04:23:10 +0300

    fix date format

--- app/src/main/java/cdu278/mangotest/ui/FormatDate.kt
+++ app/src/main/java/cdu278/mangotest/ui/FormatDate.kt
@@ -4,10 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.format
+import kotlinx.datetime.format.char
+
+private val format =
+    LocalDate.Format { dayOfMonth(); char('.'); monthNumber(); char('.'); year() }
 
 @Composable
 fun LocalDate.format(): String {
-    return remember(year, month, dayOfMonth) {
-        format(LocalDate.Formats.ISO)
-    }
+    return remember(year, month, dayOfMonth) { format(format) }
 }

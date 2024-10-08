563273feda203a1068feaad4d5d7a0e857009db6
Ildar Khalikov <rajistangosh@gmail.com>
2024-10-08 03:53:25 +0300

    add some usability in sign up screen

--- app/src/main/java/cdu278/mangotest/ui/auth/signup/SignUpScreen.kt
+++ app/src/main/java/cdu278/mangotest/ui/auth/signup/SignUpScreen.kt
@@ -3,6 +3,8 @@ package cdu278.mangotest.ui.auth.signup
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -11,12 +13,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import cdu278.mangotest.R
+import cdu278.mangotest.ui.SingleLineTextField
 import cdu278.mangotest.ui.auth.AuthScreen
 import cdu278.mangotest.ui.auth.signup.SignUpEvent.SignedUp
 import cdu278.mangotest.ui.auth.signup.SignUpUi.Error.EmptyName
@@ -55,24 +63,38 @@ fun SignUpScreen(
                 .fillMaxWidth()
         )
 
-        OutlinedTextField(
+        val nameFocusRequester = remember { FocusRequester() }
+        LaunchedEffect(null) {
+            nameFocusRequester.requestFocus()
+        }
+        SingleLineTextField(
             value = model.name,
             onValueChange = viewModel::inputName,
             label = { Text(stringResource(R.string.signUp_name)) },
             placeholder = { Text(stringResource(R.string.signUp_name_placeholder)) },
-            singleLine = true,
             isError = model.error == EmptyName,
+            keyboardOptions = KeyboardOptions(
+                capitalization = KeyboardCapitalization.Words,
+                imeAction = ImeAction.Next,
+            ),
             modifier = Modifier
                 .fillMaxWidth()
+                .focusRequester(nameFocusRequester)
         )
 
-        OutlinedTextField(
+        val isUsernameError = model.error.let { it == EmptyUsername || it == InvalidUsername }
+        SingleLineTextField(
             value = model.username,
             onValueChange = viewModel::inputUsername,
             label = { Text(stringResource(R.string.signUp_username)) },
-            placeholder = { Text(stringResource(R.string.signUp_username_placeholder)) },
-            singleLine = true,
-            isError = model.error.let { it == EmptyUsername || it == InvalidUsername },
+            supportingText = { Text(stringResource(R.string.signUp_username_supportingText)) },
+            isError = isUsernameError,
+            keyboardOptions = KeyboardOptions(
+                imeAction = if (isUsernameError) ImeAction.None else ImeAction.Done,
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { viewModel.signUp() },
+            ),
             modifier = Modifier
                 .fillMaxWidth()
         )
